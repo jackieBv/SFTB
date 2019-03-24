e21732a9e1a0c13c8664dd01b4142997f3e23287
johanna <azujoha22.jc@gmail.com>
2019-03-24 14:34:05 -0400

    agregamos el header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+ 
 
 import { AppComponent } from './app.component';
 import { LogginComponent } from './loggin/loggin.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogginComponent
+    LogginComponent,
+    HeaderComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/assets/page/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+       <title>SFTB</title>
+       <link rel="shortcut icon" href="../image/favicon.png">
+       <meta charset="utf-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1">
+	   
+       <link href="../css/bootstrap.min.css" rel="stylesheet">
+       <link href="../css/justified-nav.css" rel="stylesheet"> 
+	   <link href="../css/diseno.css" rel="stylesheet"> 
+       <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+       <script src="../js/jquery.min.js"></script>
+       <!-- incluye los componentes individuales de las filas -->
+       <script src="../js/bootstrap.min.js"></script>
+	    <!-- CSS para el menu vertical -->
+	   <link href="../css/vertical.css" rel="stylesheet">   
+	  
+	   <link href="../css/formularios.css" rel="stylesheet">
+	 
+	   <link rel="stylesheet" href="../css/jquery-ui.css">
+	   <script src="../js/jquery.js"></script>
+       <script src="../js/jquery-ui.js"></script>
+	  
+</head>
