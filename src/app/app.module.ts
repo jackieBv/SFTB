6caef9edb2e66173b49533424be15483cfaed89a
efra <ovando3750@hotmail.com>
2019-03-31 00:07:54 -0600

    reserva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 import { MenuComponent } from './shared/menu/menu.component';
-import { ReservaComponent } from './reserva/reserva.component';
+
 
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
