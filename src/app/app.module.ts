f97ced26188ac86ebbd033f85704593795039eaa
Jaquelin Benítez <jackiebenitez20@gmail.com>
2019-03-30 19:16:37 -0600

    con menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LogginComponent } from './loggin/loggin.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SliderComponent } from './shared/slider/slider.component';
+import { HomeComponent } from './shared/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogginComponent
+    FooterComponent,
+    HeaderComponent,
+    SliderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
