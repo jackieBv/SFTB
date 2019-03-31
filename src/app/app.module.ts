3b6b84db4975f3fe408e1819e9343e07130bf98f
Jaquelin Benítez <jackiebenitez20@gmail.com>
2019-03-31 00:13:11 -0600

    home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SliderComponent } from './shared/slider/slider.component';
-import { HomeComponent } from './shared/home/home.component';
+
 import { APP_ROUTES } from './app.route';
 import { ReservationComponent } from './pages/reservation/reservation.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -1,10 +1,12 @@
 import { Routes, RouterModule } from '@angular/router'; 
 import { ReservationComponent } from './pages/reservation/reservation.component';
-import { HomeComponent } from './shared/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const appRoutes: Routes = [
-    {path:'', component:HomeComponent},{path:'reservation', component:ReservationComponent},
+    {path:'', component:HomeComponent},
+    
+    {path:'reservation', component:ReservationComponent},
     {path:'contact', component:ContactComponent}
 ]; 
 export const APP_ROUTES = RouterModule.forRoot(appRoutes, {useHash:true}) 
